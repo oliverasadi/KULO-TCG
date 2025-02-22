16974cbccafd090e067847bba085a1149c4eb39f
oliverasadi <oliverasadi@hotmail.co.uk>
2025-02-22 14:57:27 +0000

    Card Hover Enlarge

---
+++ Assets/Resources/Scripts/CardHover.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class UIOnHoverEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Vector3 originalScale;
+    public float scaleFactor = 1.2f;
+    public float animationSpeed = 0.2f; // Speed of the transition
+
+    void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        StartCoroutine(ScaleTo(originalScale * scaleFactor));
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        StartCoroutine(ScaleTo(originalScale));
+    }
+
+    IEnumerator ScaleTo(Vector3 targetScale)
+    {
+        float time = 0;
+        Vector3 startScale = transform.localScale;
+
+        while (time < animationSpeed)
+        {
+            transform.localScale = Vector3.Lerp(startScale, targetScale, time / animationSpeed);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.localScale = targetScale;
+    }
+}
