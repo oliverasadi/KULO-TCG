496e3e2b19cfacd3c3ca39afb9dc8b2611c7c195
oliverasadi <oliverasadi@hotmail.co.uk>
2025-02-25 16:22:31 +0000

    Updated CardSO with more fields (Card Number/Creature Type/Evo)

--- Assets/Resources/Cards/CardSO.cs
+++ Assets/Resources/Cards/CardSO.cs
@@ -4,9 +4,18 @@
 public class CardSO : ScriptableObject
 {
     public string cardName;
-    public enum CardCategory { Creature, Spell, Evolution }
+
+    public enum CardCategory { Creature, Spell }
     public CardCategory category;
+
     public int power;
     public string effectDescription;
     public Sprite cardImage;
+
+    // NEW FIELDS
+    public string cardNumber;      // e.g., "OGN-01"
+    public string creatureType;    // e.g., "Dragon," "Beast," etc.
+
+    public enum BaseOrEvo { Base, Evolution }
+    public BaseOrEvo baseOrEvo;    // Indicates whether this card is a base creature or an evolution
 }
